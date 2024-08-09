0696fca43493cffaf408fab0638b5607d3e05251
Afshan01 <afshanalamengg@gmail.com>
2024-08-09 16:50:36 +0530

    Files are moved to admin folder

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { CssBaseline, Box, Toolbar } from '@mui/material';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Settings from './components/Settings'; // import Dashboard from './pages/Dashboard';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Profile from './pages/Profile';
-import Landlord from './pages/Landlord'; // Import the new Landlord page
-
-const drawerWidth = 240;
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminApp from './admin/AdminApp';
+import HomePage from './home/Homepage';
+// import TenantApp from './tenant/App';
+// import LandlordApp from './landlord/App';
+// import MaintenanceStaffApp from './maintenanceStaff/App';
 
 const App = () => {
-    const [open, setOpen] = useState(true);
-
     return (
         <Router>
-            <Box sx={{ display: 'flex' }}>
-                <CssBaseline />
-                <Navbar />
-                <Sidebar open={open} setOpen={setOpen} />
-                <Box
-                    component="main"
-                    sx={{
-                        flexGrow: 1,
-                        p: 3,
-                        marginLeft: { sm: open ? `${drawerWidth}px` : '60px' }, // Adjust margin based on sidebar state
-                        transition: 'margin 0.3s' // Smooth transition
-                    }}
-                >
-                    <Toolbar />
-                    <Routes>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/landlord" element={<Landlord />} /> {/* Add new route */}
-                        <Route path="/settings" element={<Settings />} />
-                    </Routes>
-                </Box>
-            </Box>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/admin/*" element={<AdminApp />} />
+                {/* <Route path="/tenant/*" element={<TenantApp />} />
+                <Route path="/landlord/*" element={<LandlordApp />} />
+                <Route path="/maintenance-staff/*" element={<MaintenanceStaffApp />} /> */}
+            </Routes>
         </Router>
     );
 };
